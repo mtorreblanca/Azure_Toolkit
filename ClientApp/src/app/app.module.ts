4ae59d20fece48e11918f2a1ac2138484d0dcb9d
Marcelo <m_torreblanca@outlook.com>
2019-05-12 21:08:33 -0500

    Sketch bing search done

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -7,16 +7,16 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavMenuComponent,
     HomeComponent,
-    CounterComponent,
-    FetchDataComponent
+    FetchDataComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
@@ -24,7 +24,7 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
     FormsModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
+      { path: 'search', component: SearchComponent },
       { path: 'fetch-data', component: FetchDataComponent },
     ])
   ],
---
+++ ClientApp/src/app/search/search.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+ @Component({
+     // tslint:disable-next-line:component-selector
+     selector: 'search',
+     templateUrl: './search.component.html',
+     styleUrls: ['./search.component.css']
+ })
+ export class SearchComponent {
+     constructor() { }
+ }
